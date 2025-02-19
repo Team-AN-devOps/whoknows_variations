2da3c470dde163e804d24cf79365086174df7baf
AishaBulalle <124045374+AishaBulalle@users.noreply.github.com>
2025-02-19 22:40:18 +0100

    added monitoring

--- src/backend/ruby/cache.rb
+++ src/backend/ruby/cache.rb
@@ -1,10 +1,10 @@
 require 'redis'
 
 def redis
-  @redis ||= Redis.new(url: "redis://redis:6379")  # Connect to Redis container by service name
+  @redis ||= Redis.new(url: "redis://redis:6379/0")  # Connect to Redis container by service name
 end
 
 use Rack::Cache,
     verbose: true,
     metastore: 'redis://redis:6379/0/metastore',   # Use 'redis' as hostname
-    entitystore: 'redis://redis:6379/0/entitystore' # Use 'redis' as hostname
+   entitystore: 'redis://redis:6379/0/entitystore' # Use 'redis' as hostname
